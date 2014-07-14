f27876c40c7720786808d1a72a1374f3e5cea2e9
Mark <Mark@boyofbeard.com>
2014-07-14 15:36:12 +0100

    Style guide added into theme and linked to same css/js/img files

--- js/build/main.js
+++ js/build/main.js
@@ -1,3 +1,82 @@
-function alertMe(){
-	alert("hello there World");
-}
+var global = (function(){
+})();
+
+
+var Helper = (function(){
+
+  // Shim for requestAnimationFrame from Paul Irishpaul ir
+  // http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame  ||
+            window.webkitRequestAnimationFrame    ||
+            window.mozRequestAnimationFrame       ||
+            function( callback ){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+  function toggleClass(element, cls){
+    if( document.documentElement.classList ){
+      element.classList.toggle(cls);
+    }
+    else{
+      var rx = new RegExp(cls,'');
+      if(rx.test(element.className)){
+        element.className = element.className.replace(rx,'');
+      }
+      else{
+        element.className += " "+cls;
+      }
+    }
+  }
+
+  function addClass(element,cls){
+    if( document.documentElement.classList ){
+      if( !element.classList.contains(cls) ){
+        element.classList.add(cls);
+      }
+    }
+    else{
+      var rx = new RegExp(cls,'');
+      if( !rx.test(element.className) ){
+        element.className += " "+cls;
+      }
+    }
+  }
+
+  function removeClass(element,cls){
+    if( document.documentElement.classList ){
+      if( element.classList.contains(cls) ){
+        element.classList.remove(cls);
+      }
+    }
+    else{
+      var rx = new RegExp(cls,'');
+      if(rx.test(element.className)){
+        element.className -= cls;
+      }
+    }
+  }
+
+  return {
+    toggleClass: toggleClass,
+    addClass: addClass,
+    removeClass: removeClass
+  };
+
+})();
+
+
+var Touch = (function(){
+  
+  var list = document.querySelectorAll('.nav_main_list, .carousel');
+
+  function enableTouch(){
+      Helper.addClass(list, "touch");
+  }
+
+  return{
+    enable: enableTouch
+  }
+
+})();
---
+++ js/build/navigation.js
@@ -0,0 +1,165 @@
+var Navigation = ( function() {
+
+  var settings = {
+    nav_open: document.querySelector('.nav_main_btn-menu'),
+    nav_close: document.querySelector('.nav_main_btn-close'),
+    nav_list: document.querySelector('.nav_main_list'),
+    openClass: "nav_main-open"
+  }
+
+  /*var nav_open = document.querySelector('.nav_main_btn-menu'),
+      nav_close = document.querySelector('.nav_main_btn-close'),
+      nav_list = document.querySelector('.nav_main_list');*/
+
+  //****** PUBLIC METHODS ********************************************** //
+
+  function publicInit(){
+    settings.nav_open.addEventListener("click", function(event){
+      event.preventDefault();
+      window.requestAnimFrame(onAnimFrame);
+    });
+
+    settings.nav_close.addEventListener("click", function(event){
+      event.preventDefault();
+      window.requestAnimFrame(onAnimFrame);
+    });
+  }
+
+
+  //****** PRIVATE METHODS ********************************************** //
+
+
+  function onAnimFrame(){
+    Helper.toggleClass(settings.nav_list, settings.openClass);
+  }
+
+  function enableTouch(){
+    Helper.addClass(settings.nav_list, "touch");
+  }
+
+  function runTouch(){
+
+    var pointerDownName = 'MSPointerDown';
+    var pointerUpName = 'MSPointerUp';
+    var pointerMoveName = 'MSPointerMove';
+
+    if(window.PointerEvent) {
+      pointerDownName = 'pointerdown';
+      pointerUpName = 'pointerup';
+      pointerMoveName = 'pointermove';
+    }
+    
+    // Simple way to check if some form of pointerevents is enabled or not
+    window.PointerEventsSupport = false;
+    if(window.PointerEvent || window.navigator.msPointerEnabled) {
+      window.PointerEventsSupport = true;
+    }
+
+    if (window.PointerEventsSupport) {
+      // Add Pointer Event Listener
+      settings.nav_list.addEventListener(pointerDownName, this.handleGestureStart, true);
+    } else {
+      // Add Touch Listener
+      settings.nav_list.addEventListener('touchstart', this.handleGestureStart, true);
+    
+      // Add Mouse Listener
+      settings.nav_list.addEventListener('mousedown', this.handleGestureStart, true);
+    }
+
+     function getGesturePointFromEvent(evt) {
+        var point = {};
+
+        if(evt.targetTouches) {
+          // Prefer Touch Events
+          point.x = evt.targetTouches[0].clientX;
+          point.y = evt.targetTouches[0].clientY;
+        } else {
+          // Either Mouse event or Pointer Event
+          point.x = evt.clientX;
+          point.y = evt.clientY;
+        }
+
+        return point;
+      }
+
+    // Handle the start of gestures
+    this.handleGestureStart = function(evt) {
+      evt.preventDefault();
+
+      if(evt.touches && evt.touches.length > 1) {
+        return;
+      }
+
+      // Add the move and end listeners
+      if (window.PointerEventsSupport) {
+        // Pointer events are supported.
+        document.addEventListener(pointerMoveName, this.handleGestureMove, true);
+        document.addEventListener(pointerUpName, this.handleGestureEnd, true);
+      } else {
+        // Add Touch Listeners
+        document.addEventListener('touchmove', this.handleGestureMove, true);
+        document.addEventListener('touchend', this.handleGestureEnd, true);
+        document.addEventListener('touchcancel', this.handleGestureEnd, true);
+    
+        // Add Mouse Listeners
+        document.addEventListener('mousemove', this.handleGestureMove, true);
+        document.addEventListener('mouseup', this.handleGestureEnd, true);
+      }
+    
+      initialTouchPos = getGesturePointFromEvent(evt);
+      console.log(getGesturePointFromEvent(evt));
+      settings.nav_list.style.transition = 'initial';
+    }.bind(this);
+
+    this.handleGestureMove = function(evt){
+      console.log("brap");
+    }.bind(this);
+
+
+    // Handle end gestures
+    this.handleGestureEnd = function(evt) {
+      evt.preventDefault();
+
+      if(evt.touches && evt.touches.length > 0) {
+        return;
+      }
+
+      isAnimating = false;
+    
+      // Remove Event Listeners
+      if (window.PointerEventsSupport) {
+        // Remove Pointer Event Listeners
+        document.removeEventListener(pointerMoveName, this.handleGestureMove, true);
+        document.removeEventListener(pointerUpName, this.handleGestureEnd, true);
+      } else {
+        // Remove Touch Listeners
+        document.removeEventListener('touchmove', this.handleGestureMove, true);
+        document.removeEventListener('touchend', this.handleGestureEnd, true);
+        document.removeEventListener('touchcancel', this.handleGestureEnd, true);
+    
+        // Remove Mouse Listeners
+        document.removeEventListener('mousemove', this.handleGestureMove, true);
+        document.removeEventListener('mouseup', this.handleGestureEnd, true);
+      }
+    
+      updateSwipeRestPosition();
+    }.bind(this);
+  }
+
+  return {
+    init: publicInit,
+    enableTouch: enableTouch,
+    runTouch: runTouch
+  };
+
+}());
+
+
+
+Navigation.init();
+
+
+//if(Modernizr.touch){
+  Navigation.enableTouch();
+  Navigation.runTouch();
+//}
